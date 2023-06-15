ff9944aff17a820b8e49bc4f5a0fd47601c7bd19
s.m.israt <smisratara25@gmail.com>
2023-06-15 20:39:18 +0600

    Style added for text,button,toast and necessary folder file created

---
+++ lib/screens/auth/login.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/cupertino.dart';
+
+class Login_Screen extends StatefulWidget {
+  const Login_Screen({Key? key}) : super(key: key);
+
+  @override
+  State<Login_Screen> createState() => _Login_ScreenState();
+}
+
+class _Login_ScreenState extends State<Login_Screen> {
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: [
+
+
+      ],
+    );
+  }
+}
---
+++ lib/style/style.dart
@@ -0,0 +1,67 @@
+import 'dart:ui';
+const colorYellow=Color.fromRGBO(255, 192, 0, 1);
+const colorRed = Color.fromRGBO(231, 28, 36, 1);
+const colorDark = Color.fromRGBO(136, 28, 32, 1);
+const colorGreen = Color.fromRGBO(33, 191, 115, 1);
+const colorBlue = Color.fromRGBO(52, 152, 219, 1.0);
+const colorOrange = Color.fromRGBO(230, 126, 34, 1.0);
+const colorWhite = Color.fromRGBO(255, 255, 255, 1.0);
+const colorDarkBlue = Color.fromRGBO(44, 62, 80, 1.0);
+const colorLightGray = Color.fromRGBO(135, 142, 150, 1.0);
+const colorLight = Color.fromRGBO(211, 211, 211, 1.0);
+const colorLightPink = Color.fromRGBO(255, 231, 211,1);
+const colorLightBlue = Color.fromRGBO(106, 199, 188,1);
+const colorLightYellow = Color.fromRGBO(255, 210, 109,1);
+
+
+TextStyle ButtonTextStyle() {
+  return TextStyle(
+    fontWeight: FontWeight.w400,
+    fontFamily: 'poppins',
+    fontSize: 14,
+  );
+}
+TextStyle AppText16Style() {
+  return TextStyle(
+    fontWeight: FontWeight.w400,
+    fontFamily: 'poppins',
+    fontSize: 16,
+  );
+}
+TextStyle AppText28Style() {
+  return TextStyle(
+    fontWeight: FontWeight.w400,
+    fontFamily: 'poppins',
+    fontSize: 28,
+  );
+}
+TextStyle AppText9Style() {
+  return TextStyle(
+    fontWeight: FontWeight.w400,
+    fontFamily: 'poppins',
+    fontSize: 9,
+  );
+}
+
+void SuccessToast(msg) {
+  Fluttertoast.showToast(
+      msg: msg,
+      toastLength: Toast.LENGTH_SHORT,
+      gravity: ToastGravity.BOTTOM,
+      timeInSecForIosWeb: 1,
+      backgroundColor: colorGreen,
+      textColor: colorWhite,
+      fontSize: 16.0);
+}
+
+void ErrorToast(msg) {
+  Fluttertoast.showToast(
+    msg: msg,
+    toastLength: Toast.LENGTH_SHORT,
+    gravity: ToastGravity.BOTTOM,
+    timeInSecForIosWeb: 1,
+    backgroundColor: colorRed,
+    textColor: colorWhite,
+    fontSize: 16.0,
+  );
+}
